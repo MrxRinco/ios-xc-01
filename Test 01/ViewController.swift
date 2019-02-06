e219807844b5284ca6170a20effeedddb0aeb40c
mrxrinc <mrxmirzaei@gmail.com>
2019-02-06 12:39:51 +0330

    first

--- Test 01/ViewController.swift
+++ Test 01/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var text = ""
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textField.becomeFirstResponder()
+        textField.delegate = self
+        
+//        let label02 = UILabel()
+//        label02.frame = CGRect(x: 50, y: 300, width: 200, height: 50)
+//        label02.text = "Hello from code!"
+//        view.addSubview(label02)
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textView.text = text
+    }
+    
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func btn(_ sender: UIButton) {
+        label.text = "Hello \(textField.text!)"
+        label.textColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
+        label.layer.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        textField.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
+    }
+    
+    
+    
+    
 
 }
 
